e9cf2f48950bcaa5c5e931ab7eae366e47ac7713
Firaol Tolessa <86101067+Firaol-Tolessa@users.noreply.github.com>
2022-12-26 10:01:59 -0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+void main() {
+ // Accept from user
+ // Use while loop to accept user input continously
+ // While(input !=0){
+    //if(input == 0)
+     // {withdraw}
+    //else 
+      //{deposite}
+  //}
+}
+
+void withdraw(int amount){
+  //...
+}
+
+void deposite(int amount){
+  //...
+}
